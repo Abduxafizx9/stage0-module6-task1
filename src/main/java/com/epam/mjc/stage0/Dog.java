9a79335402659aa37ba52fe1bbd60fcf1d35807f
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-12-25 20:34:47 +0500

    2.0

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,10 +1,15 @@
 package com.epam.mjc.stage0;
 
 public class Dog extends Animal {
-Dog()
-{
+    public Dog()
+    {
     super("brown",4,true);
-}
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
 
     public static void main(String[] args) {
         Dog dog = new Dog();
